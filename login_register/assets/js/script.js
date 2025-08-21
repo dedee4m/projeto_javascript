11bf987da896445edccbd0e19461abffa8979f7d
dedee4m <00001124574608sp@al.educacao.sp.gov.br>
2025-08-21 14:29:26 -0300

    criando evento de clique no botão login

---
+++ login_register/assets/js/script.js
@@ -0,0 +1,7 @@
+const btnlogin = document.querySelector("#btn-login");
+const btnregister = document.querySelector("#btn-login");
+
+btnlogin.addEventListener("click",(e) => {
+    console.log("cliquei no botão login");
+    
+})
